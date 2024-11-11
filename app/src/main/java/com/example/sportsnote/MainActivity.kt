c8ec3c3e2a94da7b6021753fd775096ce42dc2eb
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-11-12 07:13:22 +0900

    [Add] テキスト コンポーザブルを使用してシンプルなアプリを作成する

--- app/src/main/java/com/example/sportsnote/MainActivity.kt
+++ app/src/main/java/com/example/sportsnote/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sportsnote.ui.theme.SportsNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +30,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
+                        from = "TM",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,12 +40,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Magenta) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp)
-        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.padding(8.dp)
+        ) {
+            Text(
+                text = "Hello $name!",
+                fontSize = 80.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(24.dp)
+            )
+            Text(
+                text = from,
+                fontSize = 36.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.End)
+            )
+        }
     }
 }
 
@@ -47,6 +68,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SportsNoteTheme {
-        Greeting("Android")
+        Greeting(name = "Android", from = "TM")
     }
 }
