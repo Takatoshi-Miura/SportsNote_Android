1f25da0bde6423f49943daf23bb01c5c8d946a02
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-11-20 13:33:20 +0900

    [Add] Realm導入

---
+++ app/src/main/java/com/example/sportsnote/model/Group.kt
@@ -0,0 +1,4 @@
+package com.example.sportsnote.model
+
+class Group {
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.kapt) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.realm.gradle.plugin)
+    }
 }
