adfb2341af6f0a9bbbfdf7f794acc06a4fd58df7
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-12-04 07:48:55 +0900

    [Add] 設定画面のセルに画像を追加

--- app/src/main/java/com/example/sportsnote/ui/components/SectionedColumn.kt
+++ app/src/main/java/com/example/sportsnote/ui/components/SectionedColumn.kt
@@ -1,21 +1,26 @@
 package com.example.sportsnote.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.sportsnote.R
 
 /**
  * セクションタイトルと含有するアイテムのデータモデル
@@ -54,9 +59,6 @@ fun SectionedColumn(
                     item = item,
                     onItemClick = onItemClick
                 )
-                if (index < section.items.lastIndex) {
-                    Divider(thickness = 1.dp)
-                }
             }
 
             // セクション間の区切り
@@ -94,14 +96,23 @@ fun SectionHeader(title: String) {
  */
 @Composable
 fun SectionItem(item: String, onItemClick: (String) -> Unit) {
-    Text(
-        text = item,
-        fontSize = 16.sp,
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onItemClick(item) }
             .padding(8.dp)
-    )
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_home_black_24dp),
+            contentDescription = "Item Icon",
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.padding(horizontal = 8.dp))
+        Text(
+            text = item,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
