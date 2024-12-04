35631f1f1d7a1e68ade41db8c05ce7413c0b8a37
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-12-05 07:02:27 +0900

    [Add] 言語リソース設定を追加

--- app/src/main/java/com/example/sportsnote/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/example/sportsnote/ui/setting/SettingScreen.kt
@@ -1,6 +1,7 @@
 package com.example.sportsnote.ui.setting
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sportsnote.R
 import com.example.sportsnote.ui.components.ItemData
@@ -14,22 +15,22 @@ import com.example.sportsnote.ui.components.SectionedColumn
 fun SettingScreen() {
     val sections = listOf(
         SectionData(
-            title = "データ",
+            title = stringResource(R.string.data),
             items = listOf(
-                ItemData("データの引継ぎ", R.drawable.ic_home_black_24dp) { println("データの引継ぎ") }
+                ItemData(stringResource(R.string.data_transfer), R.drawable.ic_home_black_24dp) { println("データの引継ぎ") }
             )
         ),
         SectionData(
-            title = "ヘルプ",
+            title = stringResource(R.string.help),
             items = listOf(
-                ItemData("アプリの使い方", R.drawable.ic_home_black_24dp) { println("アプリの使い方") },
-                ItemData("お問い合わせ", R.drawable.ic_home_black_24dp) { println("お問い合わせ") }
+                ItemData(stringResource(R.string.how_to_use_this_app), R.drawable.ic_home_black_24dp) { println("アプリの使い方") },
+                ItemData(stringResource(R.string.inquiry), R.drawable.ic_home_black_24dp) { println("お問い合わせ") }
             )
         ),
         SectionData(
-            title = "システム情報",
+            title = stringResource(R.string.system_info),
             items = listOf(
-                ItemData("アプリバージョン", R.drawable.ic_home_black_24dp) { println("アプリバージョン") }
+                ItemData(stringResource(R.string.app_version), R.drawable.ic_home_black_24dp) { println("アプリバージョン") }
             )
         )
     )
