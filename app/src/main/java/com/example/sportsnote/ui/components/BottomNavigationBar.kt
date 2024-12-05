860c963cdf4562f77c476159e4974ef3e0c02357
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-12-06 07:23:38 +0900

    [Update] タブ名を変更

--- app/src/main/java/com/example/sportsnote/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/sportsnote/ui/components/BottomNavigationBar.kt
@@ -5,28 +5,32 @@ import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.example.sportsnote.R
 
 @Composable
 fun BottomNavigationBar(navController: NavController) {
     BottomNavigation {
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
-            label = { Text("Home") },
+            icon = { Icon(Icons.Filled.List, contentDescription = "Home") },
+            label = { Text(stringResource(R.string.task)) },
             selected = true,
             onClick = { navController.navigate("home") }
         )
         BottomNavigationItem(
             icon = { Icon(Icons.Filled.Home, contentDescription = "Dashboard") },
-            label = { Text("Dashboard") },
+            label = { Text(stringResource(R.string.note)) },
             selected = false,
             onClick = { navController.navigate("dashboard") }
         )
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Home, contentDescription = "Notifications") },
-            label = { Text("Notifications") },
+            icon = { Icon(Icons.Filled.DateRange, contentDescription = "Notifications") },
+            label = { Text(stringResource(R.string.target)) },
             selected = false,
             onClick = { navController.navigate("notifications") }
         )
