9d6abb005dd86cad8ace100289ead1ba6e42ba5d
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-12-11 07:39:39 +0900

    [Update] TaskViewModelに処理を移動

--- app/src/main/java/com/example/sportsnote/ui/task/TaskScreen.kt
+++ app/src/main/java/com/example/sportsnote/ui/task/TaskScreen.kt
@@ -1,45 +1,23 @@
 package com.example.sportsnote.ui.task
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.sportsnote.R
-import com.example.sportsnote.ui.components.ItemData
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sportsnote.ui.components.LazySectionedColumn
-import com.example.sportsnote.ui.components.SectionData
-
-@Composable
-fun TaskScreen() {
-    LazySectionedColumn(sections = getSections())
-}
 
 /**
- * 課題一覧データを取得
- *
- * @return List<SectionData>
+ * 課題一覧画面
  */
-private fun getSections(): List<SectionData> {
-    return listOf(
-        SectionData(
-            title = "Fruits",
-            items = listOf(
-                ItemData("Apple", R.drawable.ic_home_black_24dp) { println("Apple clicked") },
-                ItemData("Banana", R.drawable.ic_home_black_24dp) { println("Banana clicked") },
-                ItemData("Orange", R.drawable.ic_home_black_24dp) { println("Orange clicked") }
-            )
-        ),
-        SectionData(
-            title = "Vegetables",
-            items = listOf(
-                ItemData("Carrot", R.drawable.ic_home_black_24dp) { println("Carrot clicked") },
-                ItemData("Potato", R.drawable.ic_home_black_24dp) { println("Potato clicked") },
-                ItemData("Spinach", R.drawable.ic_home_black_24dp) { println("Spinach clicked") }
-            )
-        )
-    )
+@Composable
+fun TaskScreen(taskViewModel: TaskViewModel = viewModel()) {
+    val sections by taskViewModel.sections.collectAsState()
+    LazySectionedColumn(sections = sections)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewLazySectionedColumn() {
-    LazySectionedColumn(sections = getSections())
+    TaskScreen()
 }
--- app/src/main/java/com/example/sportsnote/ui/task/TaskViewModel.kt
+++ app/src/main/java/com/example/sportsnote/ui/task/TaskViewModel.kt
@@ -3,11 +3,47 @@ package com.example.sportsnote.ui.task
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sportsnote.R
+import com.example.sportsnote.ui.components.ItemData
+import com.example.sportsnote.ui.components.SectionData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
 
 class TaskViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _sections = MutableStateFlow<List<SectionData>>(emptyList())
+    val sections: StateFlow<List<SectionData>> = _sections
+
+    init {
+        loadSections()
     }
-    val text: LiveData<String> = _text
+
+    /**
+     * 課題一覧のダミーデータをロード
+     */
+    private fun loadSections() {
+        viewModelScope.launch {
+            _sections.value = listOf(
+                SectionData(
+                    title = "Fruits",
+                    items = listOf(
+                        ItemData("Apple", R.drawable.ic_home_black_24dp) { println("Apple clicked") },
+                        ItemData("Banana", R.drawable.ic_home_black_24dp) { println("Banana clicked") },
+                        ItemData("Orange", R.drawable.ic_home_black_24dp) { println("Orange clicked") }
+                    )
+                ),
+                SectionData(
+                    title = "Vegetables",
+                    items = listOf(
+                        ItemData("Carrot", R.drawable.ic_home_black_24dp) { println("Carrot clicked") },
+                        ItemData("Potato", R.drawable.ic_home_black_24dp) { println("Potato clicked") },
+                        ItemData("Spinach", R.drawable.ic_home_black_24dp) { println("Spinach clicked") }
+                    )
+                )
+            )
+        }
+    }
 }
