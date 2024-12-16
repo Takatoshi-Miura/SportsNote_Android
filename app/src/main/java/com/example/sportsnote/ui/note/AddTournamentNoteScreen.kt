1d9c66d35b3a31a4c1c6ab1505ca86d1283fee43
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-12-16 10:31:35 +0900

    [Update] 大会ノート追加画面のスクロール対応

--- app/src/main/java/com/example/sportsnote/ui/note/AddTournamentNoteScreen.kt
+++ app/src/main/java/com/example/sportsnote/ui/note/AddTournamentNoteScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
@@ -93,6 +95,7 @@ fun AddTournamentNoteScreen(onDismiss: () -> Unit) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(16.dp)
+                        .verticalScroll(rememberScrollState())
                 ) {
                     // 日付
                     DatePickerField()
