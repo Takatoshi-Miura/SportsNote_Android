4c3e4462d8e08b91a835aa04a8d7b7b6085a9708
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-12-20 06:39:26 +0900

    [Add] SharedPreferencesを一元管理するオブジェクトを追加

--- app/src/main/java/com/example/sportsnote/ui/MainActivity.kt
+++ app/src/main/java/com/example/sportsnote/ui/MainActivity.kt
@@ -3,14 +3,27 @@ package com.example.sportsnote.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.sportsnote.model.PreferencesManager
 import com.example.sportsnote.model.RealmManager
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // SharedPreferencesの初期化
+        PreferencesManager.init(applicationContext)
+
         // Realmの初期化
         RealmManager.initRealm(this)
+
+        // 初回起動の場合はユーザIDを作成
+        val isFirstLaunch = PreferencesManager.get(PreferencesManager.Keys.FIRST_LAUNCH, true)
+        if (isFirstLaunch) {
+            val uuid = UUID.randomUUID().toString()
+            PreferencesManager.set(PreferencesManager.Keys.USER_ID, uuid)
+            PreferencesManager.set(PreferencesManager.Keys.FIRST_LAUNCH, false)
+        }
 
         setContent {
             MainScreen()
