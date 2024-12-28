8fc31fcb86927b94ae70224ef9938d1faa373346
Takatoshi-Miura <takatoshi052031@gmail.com>
2024-12-28 22:25:25 +0900

    [Add] グループ削除確認ダイアログを追加

--- app/src/main/java/com/example/sportsnote/ui/group/GroupViewScreen.kt
+++ app/src/main/java/com/example/sportsnote/ui/group/GroupViewScreen.kt
@@ -11,11 +11,16 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.sportsnote.R
 import com.example.sportsnote.model.Group
+import com.example.sportsnote.ui.components.CustomAlertDialog
 import com.example.sportsnote.ui.components.header.NavigationScreenHeader
 import com.example.sportsnote.utils.Color
+import kotlinx.coroutines.launch
 
 /**
  * Group詳細画面
@@ -35,9 +40,13 @@ fun GroupViewScreen(
     appBarNavigationIcon: MutableState<(@Composable () -> Unit)?>,
     appBarRightIcon: MutableState<(@Composable () -> Unit)?>
 ) {
+    val group: Group? = viewModel.getGroupById(groupId)
+    val coroutineScope = rememberCoroutineScope()
+
+    // 入力データの状態管理
     var title by remember { mutableStateOf("") }
     var color by remember { mutableStateOf(Color.RED.id) }
-    val group: Group? = viewModel.getGroupById(groupId)
+    val showDialog = remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -57,7 +66,7 @@ fun GroupViewScreen(
                     )
                 },
                 onDelete = {
-                    viewModel.deleteGroup(groupId)
+                    showDialog.value = true
                 },
                 updateAppBar = { navigationIcon, rightIcon ->
                     appBarNavigationIcon.value = navigationIcon
@@ -72,5 +81,22 @@ fun GroupViewScreen(
                 onColorChange = { color = it }
             )
         }
+    }
+
+    // 削除確認ダイアログの表示
+    if (showDialog.value) {
+        CustomAlertDialog(
+            title = stringResource(R.string.deleteGroup),
+            message = stringResource(R.string.deleteGroupMessage),
+            confirmButtonText = stringResource(R.string.delete),
+            onConfirm = {
+                coroutineScope.launch {
+                    viewModel.deleteGroup(groupId)
+                    showDialog.value = false
+                    onBack()
+                }
+            },
+            showDialog = showDialog
+        )
     }
 }
