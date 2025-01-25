8076ee4cbd60c833bb1a131926066f74b0038335
Takatoshi-Miura <takatoshi052031@gmail.com>
2025-01-25 10:02:17 +0900

    [Add] フリーノート更新機能を実装

--- app/src/main/java/com/example/sportsnote/ui/note/FreeNoteScreen.kt
+++ app/src/main/java/com/example/sportsnote/ui/note/FreeNoteScreen.kt
@@ -10,12 +10,12 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.sportsnote.R
-import com.example.sportsnote.ui.LocalNavController
 import com.example.sportsnote.ui.components.CustomSpacerColumn
 import com.example.sportsnote.ui.components.MultiLineTextInputField
 import com.example.sportsnote.ui.components.header.NavigationScreenHeader
@@ -37,8 +37,6 @@ fun FreeNoteScreen(
 ) {
     val noteViewModel = NoteViewModel()
     val freeNote = noteViewModel.getNoteById(noteID)
-    val coroutineScope = rememberCoroutineScope()
-    val navController = LocalNavController.current
 
     // 入力データ
     var title by remember { mutableStateOf(freeNote!!.title) }
@@ -57,7 +55,7 @@ fun FreeNoteScreen(
         {
             MultiLineTextInputField(
                 title = stringResource(R.string.noteDetail),
-                defaultLines = 3,
+                defaultLines = calcMaxLines(),
                 onTextChanged = { updatedText -> detail = updatedText },
                 initialText = detail
             )
@@ -74,10 +72,14 @@ fun FreeNoteScreen(
             NavigationScreenHeader(
                 onDismiss = onBack,
                 onSave = {
-                    // TODO: 保存処理
+                    noteViewModel.saveFreeNote(
+                        noteId = noteID,
+                        title = title,
+                        detail = detail
+                    )
                 },
                 onDelete = { },
-                updateAppBar = { navigationIcon, rightIcon ->
+                updateAppBar = { navigationIcon, _ ->
                     appBarNavigationIcon.value = navigationIcon
                     appBarRightIcon.value = null
                 }
@@ -87,4 +89,19 @@ fun FreeNoteScreen(
             CustomSpacerColumn(items = inputFields)
         }
     }
+}
+
+/**
+ * 画面高さ一杯の行数を取得
+ *
+ * @return 行数
+ */
+@Composable
+private fun calcMaxLines(): Int {
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val lineHeight = 24.dp // 1行の高さ
+    val padding = 16.dp // 余白
+    val maxLines = ((screenHeight - padding) / lineHeight).toInt()
+    return maxLines
 }
