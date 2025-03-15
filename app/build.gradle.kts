dbb5b69cab975ba5d1248871bf812974318e7284
Takatoshi-Miura <takatoshi052031@gmail.com>
2025-03-15 17:39:36 +0900

    [Update] バージョンコード更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.it6210.sportsnote"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
